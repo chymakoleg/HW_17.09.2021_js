54f4763cdae75226ab8533e9da542698f0818a7a
chymakoleg <chymakoleg@gmail.com>
2021-09-18 00:19:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+for (let i = 2; i <= 14; i ++) {
+    let num = i % 2;
+    if (!num) {
+    console.log(i);
+}
+}
+
+
+
+
+for (let k = 33; k <= 45; k ++) {
+    let num = k % 2;
+    if (num) {
+    console.log(k);
+}
+}
+
+
+
